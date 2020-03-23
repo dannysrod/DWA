0a06122feaf9c1645c2b148086204fdd870f6bb0
dannysrod <dannyrod@gmail.com>
2020-03-23 15:21:10 +1100

    Linting

--- src/app/Components/weather-widget-main/weather-widget-main.component.ts
+++ src/app/Components/weather-widget-main/weather-widget-main.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeatherWidgetMainComponent implements OnInit {
 
-  WeatherData:any;
+  WeatherData: any;
   constructor() { }
 
   ngOnInit() {
@@ -19,16 +19,16 @@ export class WeatherWidgetMainComponent implements OnInit {
     console.log(this.WeatherData);
   }
 
-  getWeatherData(){
+  getWeatherData() {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=sydney&appid=99cefb99c948d2c1072dd792abe1ae92')
-    .then(response=>response.json())
-    .then(data=>{this.setWeatherData(data);})
+    .then(response => response.json())
+    .then(data =>  {this.setWeatherData(data)  ;});
 
     // let data = JSON.parse('{"coord":{"lon":72.85,"lat":19.01},"weather":[{"id":721,"main":"Haze","description":"haze","icon":"50n"}],"base":"stations","main":{"temp":297.15,"feels_like":297.4,"temp_min":297.15,"temp_max":297.15,"pressure":1013,"humidity":69},"visibility":3500,"wind":{"speed":3.6,"deg":300},"clouds":{"all":20},"dt":1580141589,"sys":{"type":1,"id":9052,"country":"IN","sunrise":1580089441,"sunset":1580129884},"timezone":19800,"id":1275339,"name":"Mumbai","cod":200}');
     // this.setWeatherData(data);
   }
 
-  setWeatherData(data){
+  setWeatherData(data) {
     this.WeatherData = data;
     let sunsetTime = new Date(this.WeatherData.sys.sunset * 1000);
     this.WeatherData.sunset_time = sunsetTime.toLocaleTimeString();
